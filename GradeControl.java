b3b3a113f0e2932caf58e0f4d012d1eb27c7f767
caleslie00 <42848797+caleslie00@users.noreply.github.com>
2020-05-03 18:41:40 -0500

    Added C notification

--- GradeControl.java
+++ GradeControl.java
@@ -21,5 +21,9 @@ public class GradeControl
         	System.out.println("You are failing!");
         	grade += 5;
         }
+        else if(grade < 70)
+        {
+        	System.out.println("You have a C, that's okay I guess.");
+        }
     }
 }

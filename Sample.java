1333dabb311c201cb520b1005d7e5d9308180d45
caleslie00 <42848797+caleslie00@users.noreply.github.com>
2020-05-03 18:27:32 -0500

    Added success case

--- Sample.java
+++ Sample.java
@@ -11,5 +11,10 @@ public class GradeControl
     public static void main( String[] args )
     {
         System.out.println( "This file is a file to show changes in the Version Control Branching assignment" );
+
+        int grade = 60;
+
+        if(grade > 90)
+        	System.out.println("Success!");
     }
 }

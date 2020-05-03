d50935c163855966aa09152575e393c7e0aab048
caleslie00 <42848797+caleslie00@users.noreply.github.com>
2020-05-03 18:21:35 -0500

    0.1

---
+++ Sample.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+*  Author : cal17b Carter Leslie
+*  Class  : Spring 2020 CS374.01 Dr. Reeves
+*  Date   : 5/6/2020
+*  Task   : Version Control Branching
+*
+******************************************************************************/
+
+public class GradeControl 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This file is a file to show changes in the Version Control Branching assignment" );
+    }
+}

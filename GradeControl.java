436693ed63d91f930c3ef95852be9b66f2aecb0f
caleslie00 <42848797+caleslie00@users.noreply.github.com>
2020-05-03 21:39:47 -0500

    More extra credit and output

--- GradeControl.java
+++ GradeControl.java
@@ -23,12 +23,15 @@ public class GradeControl
         }
 
         System.out.println("Your grade now is "+grade);
-         System.out.println("More extra credit available!");
+        System.out.println("More extra credit available!");
         grade += 3;
       
         if(grade > 90)
         	System.out.println("Success!");
         else
-        	grade += 5;
+        {
+        	System.out.println("Still not an A! Do some more extra credit.")
+        	grade +=4;
+        }
     }
 }

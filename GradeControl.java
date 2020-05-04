7f8f10f81fda9255e91f4df491134b75291997e2
caleslie00 <42848797+caleslie00@users.noreply.github.com>
2020-05-03 21:13:00 -0500

    Added B check

--- GradeControl.java
+++ GradeControl.java
@@ -18,13 +18,17 @@ public class GradeControl
         	System.out.println("Success!");
         else if(grade < 60)
         {
-        	System.out.println("You are failing!")
+        	System.out.println("You are failing!");
         	grade += 5;
         }
         else if (grade < 80)
         {
-        	System.out.println("your grade is a C")
-        	grade += 7
+        	System.out.println("Your grade is a C");
+        	grade += 7;
+        }
+        else if (grade < 90)
+        {
+        	System.out.println("you grade is a B");
         }
     }
 }

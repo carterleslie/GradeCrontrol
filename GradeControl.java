a1a3cb3757f29e83b68c616285e5e483513f8039
caleslie00 <42848797+caleslie00@users.noreply.github.com>
2020-05-03 21:02:58 -0500

    Added message for failure

---
+++ GradeControl.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+*  Author : cal17b Carter Leslie
+*  Class  : Spring 2020 CS374.01 Dr. Reeves
+*  Date   : 5/6/2020
+*  Task   : Version Control Branching
+******************************************************************************/
+
+public class GradeControl 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This file is a file to show changes in the Version Control Branching assignment" );
+
+        int grade = 60;
+
+        if(grade > 90)
+        	System.out.println("Success!");
+        else
+        {
+        	System.out.println("Fail! Do some extra credit!")
+        	grade += 5;
+        }
+    }
+}
